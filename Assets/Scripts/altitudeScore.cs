3761e56e368f68894228a853de669ee6da04f795
Conrad <conradbennett8@gmail.com>
2023-10-18 11:22:40 +0100

    Added Altitude Counter
    also made folders for scripts and prefabs

---
+++ Assets/Scripts/altitudeScore.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class altitudeScore : MonoBehaviour
+{
+    public Transform player; // put player game object in this in the editor
+    public TextMeshProUGUI altitudeText; // altitude text element on the scene
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null && altitudeText != null)
+        {
+            float altitude = player.position.y; // sets text to the players height
+            altitudeText.text = "Altitude: " + altitude.ToString("F2") + " meters"; // puts the altitude into text
+        }
+    }
+}
