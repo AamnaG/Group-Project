804d2e28e5d8fcdf1f11702c2b96553c06ca123c
Bondhan Ahmed <62183718+bondhanahmed@users.noreply.github.com>
2024-01-13 01:22:19 +0000

    Demo level added - properly transitions into main scene via portal
    - coin collection particle animation uncommented
    - main menu has to transition to demo scene

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public string sceneName;
+
+    // Once the player touches the portal
+    // the scene will transition to the new scene
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player")) {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+}
